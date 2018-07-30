832c0ef51b53921cc319745c442d79a065806bcd
Maksim Inozemtsev <maksim.inozemtsev@gmail.com>
2018-07-30 09:22:29 +0500

    feat: Add markers to the map

--- src/Map.js
+++ src/Map.js
@@ -30,9 +30,32 @@ class Map extends Component {
     this.getGoogleMaps().then(() => {
       let mapId = document.getElementById('map');
       const map = new window.google.maps.Map(mapId, { // initialize the map
-        center: {lat: 33.777858, lng: -84.391240},
+        center: {lat: 33.7774953, lng: -84.3841777},
         zoom: 14,
       });
+      // Markers -------------------------------------------------------------------------
+      const markers = [];
+      const locations = [
+        {title: 'Georgia Aquarium', location: {lat: 33.763382, lng: -84.3951098}},
+        {title: 'Atlanta Botanical Garden', location: {lat: 33.7905, lng: -84.3736}},
+        {title: 'Atlanta Symphony Orchestra', location: {lat: 33.789158, lng: -84.384935}},
+        {title: 'World of Coca-Cola', location: {lat: 33.7629, lng: -84.3927}},
+        {title: 'The Fox Theatre', location: {lat: 33.7726, lng: -84.3856}},
+        {title: 'Center For Puppetry Arts', location: {lat: 33.7926066, lng: -84.3893476}},
+        {title: 'Margaret Mitchell House', location: {lat: 33.7812918, lng: -84.3844107}}
+      ];
+      const locationsNewArray = locations.map(function (position) {
+        const title = position.title
+        const markerPosition = position.location
+        const marker = new window.google.maps.Marker({
+          map: map,
+          position: markerPosition,
+          title: title,
+          animation: window.google.maps.Animation.DROP
+        });
+        // Push the marker to the array of markers
+        markers.push(marker);
+      })
       this.setState({ map: map })
     });
   }
