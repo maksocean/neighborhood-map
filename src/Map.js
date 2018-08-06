a924f940a05dfcee52219b63f8b4063ffccd5e3d
Maksim Inozemtsev <maksim.inozemtsev@gmail.com>
2018-08-06 21:38:40 +0500

    refactor: Make a component for marker styles

--- src/Map.js
+++ src/Map.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import Options from './Options'
 import { getVenues } from './FoursquareAPI'
 import OptionsBox from './OptionsBox'
+import { colorIcon } from './MarkerStyles'
 
 
 class Map extends Component {
@@ -53,17 +54,6 @@ class Map extends Component {
     // Style markers
     const defaultMarker = colorIcon('f8737e');
     const highlightedMarker = colorIcon('fff');
-    function colorIcon(markerColor) {
-      const markerImage = new window.google.maps.MarkerImage(
-        'http://chart.googleapis.com/chart?chst=d_map_spin&chld=1.15|0|'+ markerColor +
-        '|40|_|%E2%80%A2',
-        new window.google.maps.Size(21, 34),
-        new window.google.maps.Point(0, 0),
-        new window.google.maps.Point(10, 34),
-        new window.google.maps.Size(21,34)
-      );
-      return markerImage;
-    }
     
     // Our Markers
     this.setState({mapState: mapGoogle}, (() => {
@@ -87,6 +77,7 @@ class Map extends Component {
           this.populateInfoWindow(marker, largeInfowindow, mapMarker);
         });
         // Style markers
+        colorIcon();
         window.google.maps.event.addListener(marker, "mouseover", function() {
           marker.setIcon(highlightedMarker);
         });
---
+++ src/MarkerStyles.js
@@ -0,0 +1,12 @@
+export const colorIcon = markerColor => {
+  const markerImage = new window.google.maps.MarkerImage(
+    'http://chart.googleapis.com/chart?chst=d_map_spin&chld=1.15|0|'+ markerColor +
+    '|40|_|%E2%80%A2',
+    new window.google.maps.Size(21, 34),
+    new window.google.maps.Point(0, 0),
+    new window.google.maps.Point(10, 34),
+    new window.google.maps.Size(21,34)
+  );
+  return markerImage;
+}
+
