cb51366bc0f4f9f6c3b6b11857495c1c077778b4
Maksim Inozemtsev <maksim.inozemtsev@gmail.com>
2018-08-07 07:10:47 +0500

    refactor: Move locations in a separate file

---
+++ src/Atlanta.js
@@ -0,0 +1,9 @@
+export const atlanta = [
+  {title: 'Georgia Aquarium', location: {lat: 33.763382, lng: -84.3951098}, id: '44dbbfa8f964a520a0361fe3'/*'ChIJGQT0RX4E9YgR3EqvqXZw1_4'*/},
+  {title: 'Atlanta Botanical Garden', location: {lat: 33.7905, lng: -84.3736}, id: '4a31751af964a520d7991fe3'/*'ChIJ0RXVJTgE9YgRvEWYcl5mE-Q'*/},
+  {title: 'Atlanta Symphony Hall', location: {lat: 33.789158, lng: -84.384935}, id: '4ad7ae55f964a5209e0d21e3'/*'ChIJf8bUMEUE9YgRnUmVQgl_058'*/},
+  {title: 'World of Coca-Cola', location: {lat: 33.7629, lng: -84.3927}, id: '4a49624ff964a5203fab1fe3'/*'ChIJ8yjI7H4E9YgRyacfAZqyAUQ'*/},
+  {title: 'The Fox Theatre', location: {lat: 33.7726, lng: -84.3856}, id: '49e3b8d8f964a520bb621fe3'/*'ChIJ28DQdm8E9YgRnsZ4YZ94nRo'*/},
+  {title: 'Center For Puppetry Arts', location: {lat: 33.7926066, lng: -84.3893476}, id: '4a0a672ef964a5209f741fe3'/*'ChIJ4fRXllAE9YgR6nR6SZWzinI'*/},
+  {title: 'Margaret Mitchell House', location: {lat: 33.7812918, lng: -84.3844107}, id: '4b5359d3f964a520ab9827e3'/*'ChIJC26jokIE9YgR4p_yLHsSSBE'*/}
+]
